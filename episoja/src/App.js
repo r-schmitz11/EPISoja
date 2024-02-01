02f3cbb35acc7554481445ab0deb70014bd1526d
phimhp7 <114659925+phimhp7@users.noreply.github.com>
2024-02-01 15:45:56 +0100

    feat: react init

---
+++ episoja/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sign from './component/Sign';
+
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-cyan-200 to-blue-400 h-screen w-screen grid place-items-center">
+      <Sign />
+    </div>
+  );
+}
+
+export default App;
---
+++ episoja/src/component/Sign.js
@@ -0,0 +1,9 @@
+function Sign() {
+  return (
+    <div className="relative w-1/2 h-3/5 rounded-xl bg-white mx-auto">
+
+    </div>
+  );
+}
+
+export default Sign;
