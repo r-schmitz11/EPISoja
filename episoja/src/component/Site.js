b1032651456481a2e60afd4561d0b56cd5b113f5
Enzo Lorenzini <enzo.lorenzini@epitech.eu>
2024-02-05 15:38:41 +0100

    [+] add first step for the second page in the project

--- episoja/src/component/Site.js
+++ episoja/src/component/Site.js
@@ -1,10 +1,17 @@
 function Site() {
   return (
-    <div className="bg-gradient-to-r from-slate-300 to-blue-400 h-screen w-screen">
-        <div className="bg-white w-full h-[12%] grid grid-cols-2 outline place-items-center">
-            <h1 className="col-start-1 outline">Episoja</h1>
-        </div>
-    </div>
+    <nav className="bg-sky-100 p-3 fixed w-full top-0 z-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="#" class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-sky-500 font-bold text-4xl">EPISoja</a>
+        <ul className="flex space-x-4">
+          <li><a href="#" className=" text-sky-400 hover:text-pink-500 hover:border border-solid border-pink-500 p-1 rounded-lg">Gab</a></li>
+          <li><a href="#" className=" text-sky-400 hover:text-pink-500 hover:border border-solid border-pink-500 p-1 rounded-lg">Romain</a></li>
+          <li><a href="#" className=" text-sky-400 hover:text-pink-500 hover:border border-solid border-pink-500 p-1 rounded-lg">Galerie</a></li>
+          <li><a href="#" className=" text-sky-400 hover:text-pink-500 hover:border border-solid border-pink-500 p-1 rounded-lg">Messages</a></li>
+          <li><a href="#" className=" text-sky-400 hover:text-pink-500 hover:border border-solid border-pink-500 p-1 rounded-lg">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
